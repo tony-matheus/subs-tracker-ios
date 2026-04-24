ce2c1efb453d36b1f3462e8b465552114a0280f8
Tony <mantony0148@icloud.com>
2026-04-23 19:17:45 -0500

    Initial Commit

---
+++ LinguicaSubTracker/LinguicaSubTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguicaSubTrackerApp.swift
+//  LinguicaSubTracker
+//
+//  Created by Tony Matheus on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct LinguicaSubTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LinguicaSubTrackerTests/LinguicaSubTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  LinguicaSubTrackerTests.swift
+//  LinguicaSubTrackerTests
+//
+//  Created by Tony Matheus on 23/04/26.
+//
+
+import Testing
+@testable import LinguicaSubTracker
+
+struct LinguicaSubTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
